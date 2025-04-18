1e3265860cb5bb2349dcec28e6a631deeb59d2b7
IamPierrot <phatnguyentan836@gmail.com>
2025-04-18 08:49:54 +0700

    upd: change pass lavalink

--- src/main/kotlin/database/schemas/Guilds.kt
+++ src/main/kotlin/database/schemas/Guilds.kt
@@ -5,7 +5,7 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.text
 
 object Guilds : Table<Guild>("guild") {
-    val guildId = text("guildId").primaryKey().bindTo { it.guildId }
-    val guildName = text("guildName").bindTo { it.guildName }
-    val guildOwnerId = text("guildOwner").bindTo { it.guildOwnerId }
+    val guildId = text("guild_id").primaryKey().bindTo { it.guildId }
+    val guildName = text("guild_name").bindTo { it.guildName }
+    val guildOwnerId = text("guild_owner_id").bindTo { it.guildOwnerId }
 }
